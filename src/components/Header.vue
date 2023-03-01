bcd321112f49a6d0b756721851fa0ea67bcf6c49
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-03-01 14:58:44 +0100

    creazione cartella header

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<script>
+//import
+import Header from './components/Header.vue';
+
+//export
+export default {
+  components: {
+    Header,
+  },
+
+
+}
+</script>
+
+<template></template>
+
+<style lengs="scss">
+/* regole base  */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 1200px;
+  margin: auto;
+}
+
+.flex {
+  display: flex;
+}
+
+.wrap {
+  flex-wrap: wrap;
+}
+
+img {
+  max-width: 100%;
+}
+
+ul {
+  list-style-type: none;
+}
+
+ul li {
+  margin: 0.5rem;
+}
+
+ul li a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
